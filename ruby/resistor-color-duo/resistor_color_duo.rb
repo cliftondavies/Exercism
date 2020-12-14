91cea02f3be9094ebebd9c104f0281d62747297f
cliftondavies <clifton.davies@outlook.com>
2020-12-14 17:21:00 +0000

    Iterate resistor color duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,6 +1,7 @@
 module ResistorColorDuo
+  BANDS = %w[black brown red orange yellow green blue violet grey white].freeze
+
   def self.value(colors)
-    bands = %w[black brown red orange yellow green blue violet grey white]
-    "#{bands.index(colors[0])}#{bands.index(colors[1])}".to_i
+    colors.first(2).map { |color| BANDS.index(color) }.join.to_i
   end
 end
