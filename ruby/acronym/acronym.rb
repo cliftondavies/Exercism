c82fc15930f0a6ffcb88acd6fd3e63661c5c7d55
cliftondavies <clifton.davies@outlook.com>
2021-01-07 21:24:25 +0000

    Acronym with top level comment

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,3 +1,5 @@
+# A program that converts a long name like
+# Portable Network Graphics to its acronym (PNG).
 module Acronym
   def self.abbreviate(phrase)
     phrase.split(/\W+/).reduce('') { |acronym, word| acronym << word[0] }.upcase
